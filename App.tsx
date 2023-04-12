f0dc341f1bc22841c56f0572e65bbdd80798ec2b
Andrei Dutkovsky <andreidutkovsly@yandex.ru>
2023-04-12 16:43:08 +0300

    test connect firebase

--- App.tsx
+++ App.tsx
@@ -1,59 +1,14 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, { useEffect } from 'react';
 import {
   SafeAreaView,
-  ScrollView,
   StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
+  useColorScheme,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -62,65 +17,51 @@ function App(): JSX.Element {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
-  console.log(23424)
+  useEffect(() => {
+    const fetchImgs = async () => {
+      console.log("photos");
+      // let photos = await readData("images/all");
+      const usersCollection = firestore().collection('images');
+      console.log(usersCollection);
+    }
+    fetchImgs()
+  }, []);
+
+  console.log(23424);
 
   return (
-    <SafeAreaView style={backgroundStyle}>
+    <SafeAreaView style={styles.sectionContainer}>
       <StatusBar
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-            <Text>
-            dfdfdf
-            </Text>
-            <Text>
-            dfdfdf
-            </Text>
-
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+      <View style={styles.viewStyle}>
+        <Text style={styles.textStyle}>STAR GATE</Text>
+      </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+    flex: 1,
   },
-  sectionTitle: {
-    fontSize: 24,
+  viewStyle: {
+    backgroundColor: '#30d0fe',
+    height: 116,
+    justifyContent: 'center',
+    paddingLeft: 22,
+    paddingTop: 71,
+    shadowColor: 'black',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative',
+  },
+  textStyle: {
+    fontSize: 28,
     fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+    color: '#fff',
   },
 });
 
---
+++ api/CRUD_FireBase.js
@@ -0,0 +1,29 @@
+// import {updateDoc, setDoc, deleteDoc, getDoc, doc} from 'firebase/firestore';
+// import { db } from '../fireBaseConfig';
+
+// const setData = async (url, value) => {
+//   const docRef = doc(db, url);
+//   await setDoc(docRef, value, {merge: true});
+// };
+
+// const updateData = async (url, value) => {
+//   const docRef = doc(db, url);
+//   await updateDoc(docRef, value);
+// };
+
+// const createData = async (url, value) => {
+//   const docRef = doc(db, url);
+//   await setDoc(docRef, value);
+// };
+
+// const deleteData = async (url, value) => {
+//   await deleteDoc(doc(db, url));
+// };
+
+// const readData = async url => {
+//   const docRef = doc(db, url);
+//   const docSnap = await getDoc(docRef);
+//   return docSnap.exists() ? docSnap.data() : 'No such document!';
+// };
+
+// export {readData, updateData, setData, createData, deleteData};
